f942c6733650dbb08cb5ab91aba3b695c2ca5c87
Jeffrey Chen <jeffreychen@Mac.lan>
2025-11-27 15:30:40 -0800

    fuguo qiangbing

--- hw4lib/trainers/asr_trainer.py
+++ hw4lib/trainers/asr_trainer.py
@@ -110,7 +110,7 @@ class ASRTrainer(BaseTrainer):
             transcript_lengths = transcript_lengths.to(self.device)
             with torch.autocast(device_type=self.device, dtype=torch.float16):
                 # TODO: get raw predictions and attention weights and ctc inputs from model
-                seq_out, curr_att, ctc_inputs = self.model(feats, targets_shifted, feat_lengths)
+                seq_out, curr_att, ctc_inputs = self.model(feats, targets_shifted, feat_lengths, transcript_lengths)
                 
                 # Update running_att with the latest attention weights
                 running_att = curr_att
